422f4fac9ad8a51c7e919c562ee1410b7edae354
Abdullah <abdullahrehan2010@gmail.com>
2025-01-24 14:37:08 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,9 @@
+def swap1(a, b) :
+
+    a = a ^ b
+    b=a^ b
+    a = a ^ b
+    print ("After Swapping: a = ", a, " b=", b)
+
+a  = int(input("Please enter value of a  = "))
+b  = int(input("Please enter value of b =  "))
